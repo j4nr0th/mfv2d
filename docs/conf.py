84db397bf0c17e97bce83e2ec7a57f4ddaa8f84e
Jan Roth <r0th.j4n@gmail.com>
2025-06-18 18:00:28 +0200

    docs(docs): added docs
    Initial Sphinx commit.

---
+++ docs/conf.py
@@ -0,0 +1,78 @@
+"""Configuration file for the Sphinx documentation builder."""
+
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "mfv2d"
+copyright = "2025, Jan Roth"  # noqa: A001
+author = "Jan Roth"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinxcontrib.bibtex",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+    # "sphinx_gallery.gen_gallery",
+    "jupyter_sphinx",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+
+# -- Options for autodoc extension -------------------------------------------
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "inherited-members": True,
+    "show-inheritence": True,
+}
+
+autodoc_type_aliases = {
+    "npt.NDArray": "array",
+    "npt.ArrayLike": "array_like",
+}
+
+
+# -- Options for BibTeX extension ---------------------------------------------
+bibtex_bibfiles = ["refs.bib"]
+
+# -- Options for Intersphinx -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "pyvista": ("https://docs.pyvista.org/", None),
+}
+
+
+# -- Options for Napoleon ----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_special_with_doc = True
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_attr_annotations = True
+napoleon_type_aliases = {
+    "array_like": ":obj:`numpy.array_like <numpy:array_like>`",
+    "ArrayLike": ":obj:`numpy.ArrayLike <numpy:ArrayLike>`",
+    "array": ":obj:`numpy.ndarray <numpy:ndarray>`",
+}
